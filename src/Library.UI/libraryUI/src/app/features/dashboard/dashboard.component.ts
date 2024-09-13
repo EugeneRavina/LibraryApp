60add5b162e326277491a46965211f800d4b788a
eugene <eugenejhonravina@gmail.com>
2024-09-13 13:26:39 +0800

    add book function in UI

--- src/Library.UI/libraryUI/src/app/features/dashboard/dashboard.component.ts
+++ src/Library.UI/libraryUI/src/app/features/dashboard/dashboard.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { ApiService } from '../../api.service';
+import { ApiService } from '../../services/api.service';
 import { HttpClientModule } from '@angular/common/http';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import { IBook } from '../../models/book';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HttpClientModule, MatToolbarModule, MatCardModule, CommonModule],
+  imports: [HttpClientModule, MatToolbarModule, MatCardModule, CommonModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   providers: [ApiService]  
@@ -20,9 +22,8 @@ export class DashboardComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-
     this.apiService.getData().subscribe(
-      (res: any) => {
+      (res: IBook[]) => {
         console.log(res);
         this.books = res;
       },
@@ -31,11 +32,23 @@ export class DashboardComponent implements OnInit {
       }
     )
   }
-}
 
-interface IBook {
-  id: number;
-  title: string;
-  author: string;
-  publishedDate: string;
+  addBook() : void {
+    console.log('click');
+    const book : IBook = {
+      title:"test",
+      author: "eu",
+      isbn: "string",
+      publishedDate: "2024-01-01"
+    };
+
+    this.apiService.addBook(book).subscribe(
+      (res: any) => {
+        console.log('add', res);
+      },
+      (error) => {
+        console.error('ERROR FETCHING API', error);
+      }
+    );
+  }
 }
---
+++ src/Library.UI/libraryUI/src/app/models/book.ts
@@ -0,0 +1,7 @@
+export interface IBook {
+    id?: number;
+    title: string;
+    author: string;
+    isbn: string;
+    publishedDate: string;
+}
