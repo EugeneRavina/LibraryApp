46042bfc850a04f2ca40f91a038a4a1f51ca323b
eugene <eugenejhonravina@gmail.com>
2024-09-13 12:57:06 +0800

    add cors in Library BFF

--- src/Library.BFF/Library.BFF.Api/Program.cs
+++ src/Library.BFF/Library.BFF.Api/Program.cs
@@ -3,14 +3,30 @@ using Library.BFF.Core.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<ILibraryService, LibraryService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ILibraryService, LibraryService>();
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("libraryUI",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200") // Angular app URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
+
+// Use CORS
+app.UseCors("libraryUI");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
